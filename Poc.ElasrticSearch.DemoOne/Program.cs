58da5bccc5d472bce18460537f96ce133cfff07d
Wise Kwasi DUHO <WiseK@finonex.com>
2022-07-06 22:31:41 +0000

    Add project files.

---
+++ Poc.ElasrticSearch.DemoOne/Program.cs
@@ -0,0 +1,42 @@
+using System.Reflection;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((hostBuilderContext, loggerConfiguration) =>
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(hostBuilderContext.Configuration);
+    loggerConfiguration.WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(hostBuilderContext.Configuration["ElasticConfiguration:Uri"]))
+    {
+        AutoRegisterTemplate = true,
+        IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name?.ToLower().Replace(".", "-")}-{hostBuilderContext.HostingEnvironment.EnvironmentName.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+        ,NumberOfReplicas = 1,
+        NumberOfShards = 2
+    });
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+app.UseSerilogRequestLogging();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
